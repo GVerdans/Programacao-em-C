9eaebe0a76fcefd97597ae171e6a2a5873ffbf1d
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-14 11:52:36 -0300

    add aventureiro folder

---
+++ Aventureiro/Funcoes/funcoes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+// função para entrada de dados
+void entradaDeDados(){
+
+}
+
+// calculo da média
+float calcularMedia(){
+
+}
+
+void exibirMedia(){
+
+}
+
+int main(){
+
+    entradaDeDados();
+    float media = calcularMedia();
+    exibirMedia();
+    
+    return 0;
+}
