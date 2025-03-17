142eaef4128a70201e0f5c87e1947b4ac47a07e9
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-17 14:39:07 -0300

    Organizado os diretorios

---
+++ super-trunfo-em-C/02-desenvolvendo-a-logica/01-Novato/if-else-composto/if-else*composto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1 = 30, num2 = 20;
+
+    if(num1 > num2){
+        printf("Numero 1 maior que numero 2\n");
+    } else {
+        printf("Numero 2 maior que Numero 1\n");
+    }
+
+}
