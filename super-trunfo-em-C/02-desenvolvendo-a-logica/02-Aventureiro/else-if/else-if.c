b94ae206d61c40f3441ac27daefd4ac3daeab63e
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-17 16:16:00 -0300

    done else-if

--- super-trunfo-em-C/02-desenvolvendo-a-logica/02-Aventureiro/else-if/else-if.c
+++ super-trunfo-em-C/02-desenvolvendo-a-logica/02-Aventureiro/else-if/else-if.c
@@ -2,18 +2,40 @@
 
 int main(){
 
-    int idade = 59;
+    // int idade = 59;
 
-    if(idade < 12){
-        printf("voce é uma criança!\n");
+    // if(idade < 12){
+    //     printf("voce é uma criança!\n");
 
-    } else if (idade >= 12 && idade < 18){
-        printf("Você é um adolescente\n");
+    // } else if (idade >= 12 && idade < 18){
+    //     printf("Você é um adolescente\n");
 
-    } else if(idade >= 18 && idade < 60){
-        printf("Você é um Adulto!\n");
+    // } else if(idade >= 18 && idade < 60){
+    //     printf("Você é um Adulto!\n");
 
-    } else
-        printf("Valeu idoso!\n");
+    // } else
+    //     printf("Valeu idoso!\n");
     
+
+    int nota;
+
+    printf("Digite sua nota: ");
+    scanf("%i", &nota);
+
+    if(nota >= 90){
+        printf("Conceito A\n");
+
+    } else if ("nota >= 80"){
+        printf("Conceito B\n");
+
+    } else if(nota >= 70){
+        printf("Conceito C\n");
+
+    }else if (nota >= 60){
+        printf("Conceito B\n");
+    } else {
+        printf("Burrão\n");
+    }
+
+
 }
