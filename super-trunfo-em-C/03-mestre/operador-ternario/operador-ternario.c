4421df1f5fb22ed1d9177eab2feff577e2e1140a
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-18 18:51:51 -0300

    add operador-ternario folder/file

---
+++ super-trunfo-em-C/03-mestre/operador-ternario/operador-ternario.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // int idade = 16, resultado;
+
+    //condição ? verdadeiro : falso
+    
+                // resultado = idade >= 18 ? 1 : 0;
+
+                // if (resultado == 1){
+                //     printf("Voce é maior de idade\n");
+
+                // } else {
+                //     printf("Voce é menor de idade\n");
+                // }
+
+            //  int temperatura = 10, resultado;
+
+            //  resultado = temperatura >= 30 ? 1 : 0;
+
+            //  if(resultado == 1){
+            //      printf("Está calor\n");
+
+            //  } else{
+
+            //      printf("Está Frio\n");
+            //  }
+
+            int num1 = 40, num2 = 60, maior;
+
+            num1 > num2 ? (maior = num1) : (maior = num2);
+            printf("O maior numero é: %d\n", maior);
+
+
+}
