4d292efa756b05c10e50e41de4774106fc78196e
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-20 18:45:12 -0300

    incluida pasta xadrez e pastas ede estrututas de repeticao

---
+++ xadrez/01-novato/while/while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    /*
+        while(condição) {
+        codigo
+        }
+    */ 
+
+    int i = 1;
+
+    while(i <= 10){
+        printf("%d, ", i);
+        i++;
+    }
+return 0;    
+
+}
+
