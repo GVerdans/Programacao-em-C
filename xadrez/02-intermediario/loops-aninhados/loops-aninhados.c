5da82b9606b72244af259ba790266b4492861a57
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-22 16:03:12 -0300

    terminado exemplos de loops-aninhados

--- xadrez/02-intermediario/loops-aninhados/loops-aninhados.c
+++ xadrez/02-intermediario/loops-aninhados/loops-aninhados.c
@@ -2,14 +2,23 @@
 
 int main(){
 
-    for(int i = 1; i <= 10; i++){ // Loop Externo
+    // for(int i = 1; i <= 10; i++){ // Loop Externo
 
-        printf("Tabuada de %i\n", i); 
+    //     printf("Tabuada de %i\n", i); 
         
-        for(int j = 1; j <= 10; j++){ // Loop Interno
-            printf("%i x %i = %i\n", i, j, i * j);
-        }
+    //     for(int j = 1; j <= 10; j++){ // Loop Interno
+    //         printf("%i x %i = %i\n", i, j, i * j);
+    //     }
 
+    //     printf("\n");
+    // }
+
+    int n = 5;
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
         printf("\n");
     }
 
