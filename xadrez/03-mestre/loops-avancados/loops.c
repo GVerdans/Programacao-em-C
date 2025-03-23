3950f54ee8ccbdc2965547bd7b4b7bc5c2acdd3e
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-22 23:14:06 -0300

    add continue and break in loops

--- xadrez/03-mestre/loops-avancados/loops.c
+++ xadrez/03-mestre/loops-avancados/loops.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    
-    
 
+    // for(int i = 0, j = 10; i < j; i++, j-- ){
+    //     printf("i - %d, j - %d \n", i, j);
+    // }
+
+    // for (int i = 0, j = 10; i < 5 && j > 5; i++, j--) {
+    //     printf("i = %d, j = %d\n", i, j);
+    // }
+
+    // for (int i = 0; i < 100; i += (i % 2 == 0) ? 1 : 2) {
+    //     printf("%d ", i);
+    // }
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) continue; // Pula a iteração quando i é 5
+        if (i == 10) break;    // Sai do loop quando i é 10
+        printf("%d ", i);
+    }
+
+    return 0;
 }
